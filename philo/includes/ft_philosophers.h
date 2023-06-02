13cb84118be294036e51ca4c1b51e768feaf34db
YongHyun-Yi <dldydgus1995@naver.com>
2023-06-02 15:25:51 +0900

    허용함수 헤더 추가

--- philo/includes/ft_philosophers.h
+++ philo/includes/ft_philosophers.h
@@ -6,7 +6,18 @@
 /*   By: yonghyle <yonghyle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 12:30:01 by yonghyle          #+#    #+#             */
-/*   Updated: 2023/06/02 12:30:01 by yonghyle         ###   ########.fr       */
+/*   Updated: 2023/06/02 12:45:15 by yonghyle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PHILOSOPHERS_H
+# define FT_PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
