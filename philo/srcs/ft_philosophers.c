0af3228388aeaf10edd3b62155490a9005e7d85d
YongHyun-Yi <dldydgus1995@naver.com>
2023-06-06 14:53:31 +0900

    pthread create 함수 써보는중

--- philo/srcs/ft_philosophers.c
+++ philo/srcs/ft_philosophers.c
@@ -12,6 +12,32 @@
 
 #include "ft_philosophers.h"
 
+int init_philo(int num_of_philo, int die, int eat, int sleep, int num_of_time)
+{
+	// pthread_create();
+	return (0);
+}
+
+void *my_tread_routine(void *args)
+{
+	printf("Hi! I'm Thread!\n");
+	printf("args: %d\n", *((int *)args));
+	sleep(1);
+	return 0;
+}
+
+// void *test(void *data)
+// {
+//     int i;
+//     int a = *(int *)data;
+//     for (i = 0; i < 10; i++)
+//     {
+//         printf("%d\n", i*a);
+//     }
+//     return (void *)(i * 100);
+// }
+
+
 int main(int argc, char **argv)
 {
 	(void)argc;
@@ -19,4 +45,29 @@ int main(int argc, char **argv)
 	struct timeval a;
 	gettimeofday(&a, NULL);
 	printf("now is : %ld\n", a.tv_sec);
+
+	int my_arg = 1;
+	pthread_t my_thread;
+	pthread_create(&my_thread, NULL, my_tread_routine, (void *)&my_arg);
+	pthread_join(my_thread, NULL); // 이게 없으면 스레드의 함수가 진행되기도전에 main의 코드가 끝나서 프로세스가 종료되어버린다
+
+	// init_philo(int num_of_philo, int die, int eat, int sleep, int num_of_time);
+	return (0);
+
+	// int a = 100;
+    // pthread_t thread_t;
+    // int status;
+
+    // // 쓰레드를 생성한다. 
+    // if (pthread_create(&thread_t, NULL, test, (void *)&a) < 0)
+    // {
+    //     perror("thread create error:");
+    //     exit(0);
+    // }
+
+    // // 쓰레드가 종료되기를 기다린후 
+    // // 쓰레드의 리턴값을 출력한다. 
+    // pthread_join(thread_t, (void **)&status);
+    // printf("Thread End %d\n", status);
+    // return 1;
 }
