87e7f03aea3ef91a3b743420844ab457de5e4d41
YongHyun-Yi <dldydgus1995@naver.com>
2023-06-06 15:21:16 +0900

    구조체 메모중...

--- philo/srcs/ft_philosophers.c
+++ philo/srcs/ft_philosophers.c
@@ -41,18 +41,6 @@ void *my_tread_routine2(void *args)
 	return 0;
 }
 
-// void *test(void *data)
-// {
-//     int i;
-//     int a = *(int *)data;
-//     for (i = 0; i < 10; i++)
-//     {
-//         printf("%d\n", i*a);
-//     }
-//     return (void *)(i * 100);
-// }
-
-
 int main(int argc, char **argv)
 {
 	(void)argc;
@@ -71,21 +59,4 @@ int main(int argc, char **argv)
 
 	// init_philo(int num_of_philo, int die, int eat, int sleep, int num_of_time);
 	return (0);
-
-	// int a = 100;
-    // pthread_t thread_t;
-    // int status;
-
-    // // 쓰레드를 생성한다. 
-    // if (pthread_create(&thread_t, NULL, test, (void *)&a) < 0)
-    // {
-    //     perror("thread create error:");
-    //     exit(0);
-    // }
-
-    // // 쓰레드가 종료되기를 기다린후 
-    // // 쓰레드의 리턴값을 출력한다. 
-    // pthread_join(thread_t, (void **)&status);
-    // printf("Thread End %d\n", status);
-    // return 1;
 }
