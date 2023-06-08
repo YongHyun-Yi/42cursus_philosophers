6b24fa686fbef7a9a6c498effa8d8d22f8403f3e
YongHyun-Yi <dldydgus1995@naver.com>
2023-06-08 13:37:14 +0900

    이전에 발생한것은 데이터레이스가 아니라 밀려서 출력되는것, context switching이 더 짧은 주기로도 나타날수 있음을 명심할것..., 최적화 플래그로 인해 데이터레이스가 발생하지 않았었음...수정

--- philo/srcs/ft_philosophers.c
+++ philo/srcs/ft_philosophers.c
@@ -13,6 +13,7 @@
 #include "ft_philosophers.h"
 
 pthread_mutex_t my_mutex;
+int my_var = 0;
 
 int init_philo(int num_of_philo, int die, int eat, int sleep, int num_of_time)
 {
@@ -22,31 +23,56 @@ int init_philo(int num_of_philo, int die, int eat, int sleep, int num_of_time)
 
 void *my_tread_routine(void *args)
 {
-	while (1)
+	// while (1)
+	// {
+	// 	pthread_mutex_lock(&my_mutex);
+	// 	printf("Hi! I'm Thread!\n");
+	// 	printf("args: %d\n", *((int *)args));
+	// 	*(int *)args += 1;
+	// 	pthread_mutex_unlock(&my_mutex);
+	// 	usleep(1000 * 100);
+	// 	// sleep(1);
+	// }
+	for (int i = 0; i < 50000; i++)
 	{
-		pthread_mutex_lock(&my_mutex);
-		printf("Hi! I'm Thread!\n");
-		printf("args: %d\n", *((int *)args));
-		*(int *)args += 1;
-		pthread_mutex_unlock(&my_mutex);
-		usleep(1000 * 100);
-		// sleep(1);
+		// printf("my arg: %d\n", *(int *)args);
+		// *(int *)args += 1;
+		my_var++;
+		// usleep(100);
 	}
+	// {
+		// pthread_mutex_lock(&my_mutex);
+		// *(int *)args += 1;
+		// usleep(100);
+		// pthread_mutex_unlock(&my_mutex);
+	// }
 	return 0;
 }
 void *my_tread_routine2(void *args)
 {
-	while (1)
+	// while (1)
+	// {
+	// 	pthread_mutex_lock(&my_mutex);
+	// 	printf("Hi! I'm Thread2!\n");
+	// 	printf("args: %d\n", *((int *)args));
+	// 	*(int *)args -= 1;
+	// 	pthread_mutex_unlock(&my_mutex);
+	// 	usleep(1000 * 100);
+	// 	// sleep(1);
+	// 	// pthread_exit(NULL); // exit 를 쓰면 프로세스가 종료되어버린다 pthread_exit 는 스레드만 종료시킨다
+	// }
+	for (int i = 0; i < 50000; i++)
 	{
-		pthread_mutex_lock(&my_mutex);
-		printf("Hi! I'm Thread2!\n");
-		printf("args: %d\n", *((int *)args));
+		// printf("my arg: %d\n", *(int *)args);
 		*(int *)args -= 1;
-		pthread_mutex_unlock(&my_mutex);
-		usleep(1000 * 100);
-		// sleep(1);
-		// pthread_exit(NULL); // exit 를 쓰면 프로세스가 종료되어버린다 pthread_exit 는 스레드만 종료시킨다
+		// usleep(100);
 	}
+	// {
+		// pthread_mutex_lock(&my_mutex);
+		// *(int *)args -= 1;
+		// usleep(100);
+		// pthread_mutex_unlock(&my_mutex);
+	// }
 	return 0;
 }
 
@@ -61,18 +87,23 @@ int main(int argc, char **argv)
 	pthread_mutex_init(&my_mutex, NULL);
 
 	int my_arg = 0;
+	// int *my_var = malloc(sizeof(int));
+	// *my_var = 0;
 	pthread_t my_thread;
 	pthread_t my_thread2;
-	pthread_create(&my_thread, NULL, my_tread_routine, (void *)&my_arg);
-	usleep(500);
-	pthread_create(&my_thread2, NULL, my_tread_routine2, (void *)&my_arg);
+	pthread_create(&my_thread, NULL, my_tread_routine, (void *)&my_var);
+	// usleep(500);
+	pthread_create(&my_thread2, NULL, my_tread_routine2, (void *)&my_var);
 
 	pthread_t pt_arr[2];
 	pt_arr[0] = my_thread;
 	pt_arr[1] = my_thread2;
 
+
 	for (int i = 0; i < 2; i++)
-		pthread_join(pt_arr[0], NULL);
+		pthread_join(pt_arr[i], NULL);
+	// pthread_join(pt_arr[0], NULL);
+	// pthread_join(pt_arr[1], NULL);
 	/*
 		join 을 미리 써놓고 스레드의 반환값을 기다리는게 아닌거같음
 		for 문에서 돌려버려도 그전에 끝나있으면 알아서 인식하는듯
@@ -83,5 +114,6 @@ int main(int argc, char **argv)
 	// pthread_join(my_thread2, NULL);
 
 	// init_philo(int num_of_philo, int die, int eat, int sleep, int num_of_time);
+	printf("my_var: %d\n", my_var);
 	return (0);
 }
